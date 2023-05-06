a1b8239276ae8c4a0746d63060e5c63f4d30fef1
aacheng <60862865+aacheng0228@users.noreply.github.com>
2023-05-05 21:06:35 -0700

    eyes

---
+++ sketch.js
@@ -0,0 +1,25 @@
+let s = "EYES";
+
+function setup() {
+  createCanvas(1920, 1080);
+  textSize(50);
+  textAlign(CENTER, CENTER);
+}
+
+function draw() {
+  
+
+  let x = int(random(-150, width));
+  let y = int(random(-150, height));
+  let w = int(random(50, 150));
+  let h = int(random(50, 150));
+  let g = int(random(0, 255));
+  if(mouseIsPressed){
+  textSize((w + h) / 5);
+  fill(g);
+  rect(x, y, w, h);
+  fill(0);
+  text(s, x + w / 2, y + h / 2);
+}
+}
+
